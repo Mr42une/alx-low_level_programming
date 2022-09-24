4a56b7b55d79ef19c7f754abc03fe2473f7c360c
Fortune <fortune.ebiala@uniben.edu>
2022-09-23 20:19:51 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ * Return: the resulting string
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
